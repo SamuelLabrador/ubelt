0d40647924399a7b7637850b5ef14d4f1fef7505
SamuelLabrador <sam.labrador@Yahoo.com>
2020-03-12 17:05:03 -0700

    removing old tests

--- old_tests/test_color.py
+++
@@ -1,6 +0,0 @@
-def test_unable_to_find_color():
-    import ubelt as ub
-    import pytest
-    with pytest.warns(UserWarning):
-        text = ub.color_text('text', 'wizbang')
-        assert text == 'text', 'bad colors should pass the text back'
